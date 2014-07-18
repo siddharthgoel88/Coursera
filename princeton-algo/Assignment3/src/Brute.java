8243c965ee9da90010d4e6c15c050f4173a53393
Siddharth Goel <siddharth98391@gmail.com>
2014-07-18 18:51:56 +0530

    [Princeton Algorithms - 1] Assignment 3
    Almost non-working code
    
    Signed-off-by: Siddharth Goel <siddharth98391@gmail.com>

---
+++ princeton-algo/Assignment3/src/Brute.java
@@ -0,0 +1,51 @@
+
+public class Brute {
+	private Point [] pts;
+
+	public static void main(String[] args) {
+		if(args.length != 1)
+		{
+			StdOut.println("Usage: java Brute <input file>");
+			System.exit(1);
+		}
+		
+		String filename = args[0];
+		Brute obj = new Brute();
+		obj.readInput(filename);
+		obj.findCollinearPoints();
+	}
+
+	private void findCollinearPoints() {
+		int size = pts.length;
+		StdOut.println(size);
+		for (int i = 0; i < size-3; i++ ) 
+			for (int j = i+1; j < size-2; j++)
+				for (int k = j+1; k < size-1; k++)
+					for(int l = k+1; l < size; l++) {
+						if((pts[i].slopeTo(pts[j]) == pts[i].slopeTo(pts[k])) && 
+								(pts[i].slopeTo(pts[k])	== pts[i].slopeTo(pts[l]))) {
+							StdOut.println(pts[i].toString() + " -> " +
+									pts[j].toString() + " -> " +
+									pts[k].toString() + " -> " +
+									pts[l].toString() );
+						
+						}
+					}
+	}
+
+
+	private void readInput(String filename) {
+		In file = new In(filename);
+		int size,x,y;
+		String[] input;
+		size = Integer.parseInt(file.readString().trim());
+		input = file.readAllStrings();
+		pts = new Point[size];
+		while(size-- > 0) {
+			x = Integer.parseInt(input[(2*size)]);
+			y = Integer.parseInt(input[(2*size) + 1]);
+			pts[size] = new Point(x,y);
+		}
+	}
+
+}
---
+++ princeton-algo/Assignment3/src/Fast.java
@@ -0,0 +1,67 @@
+import java.util.Arrays;
+
+public class Fast {
+
+	private Point [] pts, copy;
+	
+	public static void main(String[] args) {
+		if(args.length != 1)
+		{
+			StdOut.println("Usage: java Brute <input file>");
+			System.exit(1);
+		}
+		
+		String filename = args[0];
+		Fast obj = new Fast();
+		obj.readInput(filename);
+		obj.findCollinearPoints();
+	}
+
+	private void findCollinearPoints() {
+		int size = pts.length;
+		int count=1, start=1, end=1;
+		for (int i = 0; i < size-1; i++) {
+			StdOut.println(pts[i].toString());
+			Arrays.sort(pts, pts[i].SLOPE_ORDER);
+			StdOut.println(pts[0].toString());
+			for (int j = 1; j < size-1; j++) {
+				if (pts[0].slopeTo(pts[j]) == pts[0].slopeTo(pts[j+1])) {
+					if (count == 1)
+						start = j;
+					count++;
+					if (j == size-2)
+						checkPrint(start,end);
+				} else {
+					end = j;
+					checkPrint(start,end);
+					count = 1;
+				}
+			}
+		}
+	}
+
+	private void checkPrint(int start, int end) {
+		if (end - start > 1) {
+			StdOut.print(pts[0].toString());
+			while (start <= end)
+				StdOut.print(" -> " + pts[start++].toString());
+			StdOut.println();
+		}
+	}
+
+	private void readInput(String filename) {
+		In file = new In(filename);
+		int size,x,y;
+		String[] input;
+		size = Integer.parseInt(file.readString().trim());
+		input = file.readAllStrings();
+		pts = new Point[size];
+		while(size-- > 0) {
+			x = Integer.parseInt(input[(2*size)]);
+			y = Integer.parseInt(input[(2*size) + 1]);
+			pts[size] = new Point(x,y);
+		
+		}
+	}
+	
+}
